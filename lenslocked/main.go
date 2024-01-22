dfbcffcb76282623706105a59d4c9bdd389144a9
Roman Leontovych <lordwerneo@gmail.com>
2024-01-22 16:10:06 +0200

    Lessomn_13. Added new page and routing to that page.

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -5,14 +5,20 @@ import (
 	"net/http"
 )
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Just H1 tag</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch email me at <a href=\"mailto:email@email.com\">email@email.com</a></p>")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 
 	fmt.Println("Starting server at :8000")
 	http.ListenAndServe(":8000", nil)
-}
-
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	fmt.Fprint(w, "<h1>Just H1 tag</h1>")
 }
