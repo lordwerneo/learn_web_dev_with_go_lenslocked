a5fc4c9523d4ad6b62a64166dbc80820f428d179
Roman Leontovych <lordwerneo@gmail.com>
2024-01-22 13:49:27 +0200

    Lesson_1. Created basic server.

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", handlerFunc)
+
+	fmt.Println("Starting server at :8000")
+	http.ListenAndServe(":8000", nil)
+}
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Just H1 tag</h1>")
 }
